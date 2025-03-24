b99bc008998e17c944396ac1f48863bd8164e84c
GaelFG17 <gaelfg1720@gmail.com>
2025-03-24 12:11:49 -0600

    finalizacion de las rutas

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -2,3 +2,9 @@ const {ProductsController} = require('../controller/productsController')
 const router = require('express').Router()
 
 router.get('/', ProductsController.getAllProductsController)
+router.get('/:id', ProductsController.getAllProductByIdController)
+router.post('/', ProductsController.createProductController)
+router.put('/:id', ProductsController.updateProductController)
+router.delete('/:id', ProductsController.deletProductController)
+
+module.exports = router
