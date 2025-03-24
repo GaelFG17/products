2915d2fece5f1fc7448b7f7f2700553eccc83a0e
GaelFG17 <gaelfg1720@gmail.com>
2025-03-24 12:12:31 -0600

    implementacion de las rutas

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {connectDb} = require('./data/config');
 const cors = require('cors');
+const productRouter = require('./routes/productsRoutes');
 
 const PORT = 3001;
 
@@ -9,6 +10,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 connectDb();
+
+app.use('/products', productRouter);
 
 app.listen(PORT, () => {
   console.log('Server running in http://localhost:'+PORT);
